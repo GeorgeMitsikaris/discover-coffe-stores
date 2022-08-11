06964e319a58a14d3195af7b561e79e55bf56a50
George Mitsikaris <george.mitsikaris@gmail.com>
2022-08-11 14:07:38 +0300

    feat: Created Banner component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import "../styles/globals.css";
-import Footer from "./footer";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Component {...pageProps} />
-			<Footer />
 		</>
 	);
 }
